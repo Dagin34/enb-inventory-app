067a83fd4676131159cb1b6f9b689f125aa9170e
dagin34 <dagmawinapoleon02@gmail.com>
2026-05-22 16:06:45 +0300

    More setup configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '', // Leave empty if default HTTPS port (443)
+        pathname: '**', // Allow any path under this hostname
+      },
+    ],
+  },
 };
 
 export default nextConfig;
